d678901466d4c518ea95d3c11b44b601d24464a2
gorgitko <fg-42@seznam.cz>
2016-12-25 20:28:26 +0100

    added different builds of shared model

--- activity/train-shared.py
+++ activity/train-shared.py
@@ -26,9 +26,9 @@ def batch_generator_multi(X_list, y, batch_size):
                    Xy[3])
 
 
-def build_model_shared(input_shape, n_hidden, consume_less="cpu"):
+def build_model_shared_a(input_shape, n_hidden, consume_less="cpu"):
     """
-    Build a shared GRU model: (GRU-GRU-GRU)-Dense
+    Build a shared GRU model: (GRU-GRU-GRU)-Merge-GRU-Dense
     Output from shared layer is summed.
     Taken from https://keras.io/getting-started/functional-api-guide/#shared-layers
 
@@ -43,21 +43,59 @@ def build_model_shared(input_shape, n_hidden, consume_less="cpu"):
     Model
     """
 
-    gru_1 = Input(shape=input_shape)
-    gru_2 = Input(shape=input_shape)
-    gru_3 = Input(shape=input_shape)
+    input_1 = Input(shape=input_shape)
+    input_2 = Input(shape=input_shape)
+    input_3 = Input(shape=input_shape)
 
-    shared_gru = GRU(n_hidden, consume_less=consume_less)
+    shared_gru_1 = GRU(n_hidden, return_sequences=True, consume_less=consume_less)
+    gru_1_a = shared_gru_1(input_1)
+    gru_1_b = shared_gru_1(input_2)
+    gru_1_c = shared_gru_1(input_3)
 
-    encoded_1 = shared_gru(gru_1)
-    encoded_2 = shared_gru(gru_2)
-    encoded_3 = shared_gru(gru_3)
+    shared_gru_2 = GRU(n_hidden, consume_less=consume_less)
+    gru_2_a = shared_gru_2(gru_1_a)
+    gru_2_b = shared_gru_2(gru_1_b)
+    gru_2_c = shared_gru_2(gru_1_c)
 
-    merged_vector = merge([encoded_1, encoded_2, encoded_3], mode="sum", concat_axis=-1)
-    gru = GRU(n_hidden, consume_less=consume_less)(merged_vector)
-    predictions = Dense(1, activation="sigmoid")(gru)
+    merged_vector = merge([gru_2_a, gru_2_b, gru_2_c], mode="sum")
+    predictions = Dense(1, activation="sigmoid")(merged_vector)
 
-    model = Model(input=[gru_1, gru_2, gru_3], output=predictions)
+    model = Model(input=[input_1, input_2, input_3], output=predictions)
+    model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+
+def build_model_shared_b(input_shape, n_hidden, consume_less="cpu"):
+    """
+    Build a shared GRU model: (GRU-GRU-GRU)-(GRU-GRU-GRU)-Merge-Dense
+    Output from shared layer is summed.
+    Taken from https://keras.io/getting-started/functional-api-guide/#shared-layers
+
+    Parameters
+    ----------
+    input_shape
+    n_hidden
+    consume_less
+
+    Returns
+    -------
+    Model
+    """
+
+    input_1 = Input(shape=input_shape)
+    input_2 = Input(shape=input_shape)
+    input_3 = Input(shape=input_shape)
+
+    shared_gru_1 = GRU(n_hidden, return_sequences=True, consume_less=consume_less)
+
+    gru_1_a = shared_gru_1(input_1)
+    gru_1_b = shared_gru_1(input_2)
+    gru_1_c = shared_gru_1(input_3)
+
+    merged_vector = merge([gru_1_a, gru_1_b, gru_1_c], mode="sum")
+    gru_2 = GRU(n_hidden, consume_less=consume_less)(merged_vector)
+    predictions = Dense(1, activation="sigmoid")(gru_2)
+
+    model = Model(input=[input_1, input_2, input_3], output=predictions)
     model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
     return model
 
@@ -66,14 +104,14 @@ if __name__ == "__main__":
     # data
     #root_dir = "/storage/brno2/home/jirinovo/_school/pdd/activity/"
     root_dir = ""
-    input_file_X = "{}data/data-117k-X-split_part{{}}.npy".format(root_dir)
-    input_file_y = "{}data/data-117k-y.npy".format(root_dir)
-    model_name = "activity_model-3_shared-117k-grid-150_smiles"
+    input_file_X = "{}data/data-117k-X-50_smiles-split_part{{}}.npy".format(root_dir)
+    input_file_y = "{}data/data-117k-y-50_smiles.npy".format(root_dir)
+    model_name = "activity_model-3_shared_a-117k-grid-50_smiles"
 
-    X_1 = np.load(input_file_X.format("0"))#[0:1000]
-    X_2 = np.load(input_file_X.format("1"))#[0:1000]
-    X_3 = np.load(input_file_X.format("2"))#[0:1000]
-    y = np.load(input_file_y)#[0:1000]
+    X_1 = np.load(input_file_X.format("0"))
+    X_2 = np.load(input_file_X.format("1"))
+    X_3 = np.load(input_file_X.format("2"))
+    y = np.load(input_file_y)
 
     # network params
     batch_size = 128
@@ -107,7 +145,7 @@ if __name__ == "__main__":
         early_stopping = EarlyStopping(monitor="val_loss", patience=3, verbose=5, mode="auto")
         model_checkpoint = ModelCheckpoint("{}results/{}-{}_hidden-best_weights_{{epoch:02d}}_{{val_loss:.5f}}.hdf5".format(root_dir, model_name, n_hidden),
                                            monitor="val_loss", verbose=5, save_best_only=True, mode="auto")
-        model = build_model_shared(n_hidden, input_shape)
+        model = build_model_shared_a(input_shape, n_hidden)
         history = model.fit_generator(batch_generator_multi([X_1_train, X_2_train, X_3_train], y_train, batch_size),
                                       len(X_1_train),
                                       n_epochs,
